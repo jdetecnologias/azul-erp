da985594d84afed38bc4d448279cfa02507c273e
juliocesar.alves <juliowebdesigner@hotmail.com>
2019-07-28 20:12:01 +0000

    issue5 #5 - alteracoes que foram esquecidas de empurrar

--- backend/src/funcoes/atualizarStatusVenda.js
+++ backend/src/funcoes/atualizarStatusVenda.js
@@ -10,13 +10,11 @@ MongoClient.connect(url, function(err, dbk) {
   var dbo = dbk.db("azul");
   var myquery ={_id: ObjectId(dados._id)};
   var newvalues = { $set: {status: dados.status} };
-  var oldStatus
 
   dbo.collection("vendas").updateOne(myquery, newvalues, function(err,res) {
 	 if(err) throw err
 	 dbo.collection("vendas").find(myquery).toArray(function(err,result){
 		if(err)  throw err
-		result.oldStatus = oldStatus
 		callback(result)
 	 })
 	
