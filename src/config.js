a213e5f4498e8e21b09dc1a78e5bc5d487044b27
juliocesar.alves <juliowebdesigner@hotmail.com>
2019-09-06 03:33:32 +0000

    config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	port: 3003,
 	name: 'Azul ERP',
 	env:  'development',
-	base_url: 'http://localhost:3003',
+	base_url: 'https://azul-api.herokuapp.com',
 	db: {
 		uri: process.env.MONGODB_URI || 'mongodb+srv://azul-erp:azulerp@azulerp-x6r9p.mongodb.net/azul?retryWrites=true&w=majority',
 	}
